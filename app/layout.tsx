e3b7f49573508fdd57079bcd73472b9a127e2420
rafaapcode <rafaap2003@gmail.com>
2024-03-10 16:30:31 -0300

    feat: creating the design of header'

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Ubuntu } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { cn } from "@/lib/utils";
 
 const ubuntu = Ubuntu({ subsets: ["latin"], weight: ["500", "300", "400", "700"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={ubuntu.className}>
+      <body className={cn("w-full h-full bg-[#000000] text-white", ubuntu.className)}>
         <Header />
         {children}
       </body>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { MenuIcon, SearchIcon } from "lucide-react";
+
 export default function Header() {
     return (
         <header className="w-full">
-            <div className="container mx-auto">
-                teste
+            <div className="container mx-auto flex items-center justify-between p-5">
+                <div className="relative w-11 h-7 md:w-20 md:h-14">
+                    <Link href="/">
+                        <Image fill src={"/icone.svg"} alt="logo" className="object-cover" />
+                    </Link>
+                </div>
+                <div className="flex">
+                    <Input type="text" placeholder="Pesquise sua barbearia" className="w-[140px] md:w-[200px] lg:w-[288px] text-xs md:text-sm bg-transparent border border-[#282828] placeholder:text-[#282828]" />
+                    <Button variant={"ghost"} className="ml-3">
+                        <SearchIcon className="w-5 h-5" />
+                    </Button>
+                </div>
+                <div className="hidden md:block">
+                    <Link href={"#"} className="text-sm md:text-base transition-colors duration-200 text-[#282828] hover:text-[#414040]">Agendamentos</Link>
+                    <Button variant={"ghost"} className="text-sm md:text-base ml-5">Login</Button>
+                </div>
+                <div className="md:hidden">
+                    <Button variant={"ghost"}>
+                        <MenuIcon />
+                    </Button>
+                </div>
             </div>
         </header>
     )
