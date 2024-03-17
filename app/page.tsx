e60027f8409731ca342196788c6b8712492036ce
rafaapcode <rafaap2003@gmail.com>
2024-03-17 15:12:58 -0300

    feat: adapting the interface to mobile

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import BarberShopCard from "@/components/BarberShopCard";
 export default function Home() {
   return (
     <main className="w-full">
-      <div className="container mx-auto p-5 lg:p-0">
+      <div className="container mx-auto p-5">
         <h1 className="text-lg md:text-2xl mt-[32px] md:mt-[60px] lg:mt-[38px]">Barbearias mais próximas de você</h1>
-        <div className="mt-[32px] mb-10 md:mt-[79px] lg:mt-[38px] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-5 md:gap-x-[70px] lg:gap-x-6 w-full">
+        <div className="mt-[32px] mb-10 md:mt-[79px] lg:mt-[38px] w-[80%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-y-5 md:gap-x-[70px] lg:gap-x-6">
           <BarberShopCard nome="Teste" avaliacao={10}/>
           <BarberShopCard nome="Teste" avaliacao={10}/>
           <BarberShopCard nome="Teste" avaliacao={10}/>
--- components/BarberShopCard.tsx
+++ components/BarberShopCard.tsx
@@ -15,11 +15,11 @@ export default function BarberShopCard({ avaliacao, nome }: BarberShopCardProps)
                 name: nome
             }
         }} className="group w-fit mx-auto lg:mx-0">
-            <div className="relative w-[350px] h-[230px] rounded-md">
+            <div className="relative w-[280px] lg:w-[350px] h-[230px] rounded-md">
                 <div className="absolute w-full h-full z-10 bg-black/55 group-hover:bg-black/40 transition-colors duration-300" />
                 <Image fill src={"/barberimg.webp"} alt="baber image" className="object-cover rounded-sm" />
                 <div className="relative w-full h-full z-20 bg-transparent">
-                    <div className="absolute bottom-2 left-2  w-[320px]">
+                    <div className="absolute bottom-2 left-2 w-[260px] lg:w-[320px]">
                         <div className="w-full flex justify-between items-center px-5 py-3 rounded-lg bg-white/30 backdrop:blur-lg">
                             <h2 className="text-base text-white">Nome Barbearia</h2>
                             <FeedBackBadge avaliacao={avaliacao}/>
